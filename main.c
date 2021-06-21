535ddf40b3a679325bc37f05674c6d79aba61ab1
maham912 <64104722+maham912@users.noreply.github.com>
2021-06-21 10:36:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Define structures and global variables*/
+/* Defining structures and global variables*/
 struct node {
 	int parent;
 	int first_child;
@@ -13,16 +13,13 @@ int num_processes;
 
 typedef struct node pcb_type;
 
-/***************************************************************/
 void parameter() {
 
-	/* Declare local variables */
 
-	/* Prompt for maximum number of processes */
 	printf("Enter the maximum number of processes: ");
 	scanf("%d", &num_processes);
 	
-	/* Allocate memory for dynamic array of PCB's */
+	/* Allocating memory for dynamic array of Process control blocks (PCB's) */
 	pcb = (pcb_type*)malloc(num_processes * sizeof(pcb_type));
 	
 	/* Initialize PCB[0] */
@@ -31,7 +28,7 @@ void parameter() {
 	pcb[0].older_sibling = -1;
 	pcb[0].younger_sibling = -1;
 	
-	/* Intitialize all other PCBs' parent, first_child, younger_sibiling, older_sibling */
+	/* Intitializing all other PCBs' parent, first_child, younger_sibiling, older_sibling */
 	for (int i = 1; i < num_processes; i++)
 	{
 		pcb[i].parent = -1;
@@ -44,17 +41,13 @@ void parameter() {
 }
 
 
-/***************************************************************/
 void printr() {
     
-	/* Declare local variables */
 	int i;
-
     printf("i\tParent\tFirst\tOlder\tYounger\n");
     printf("----------------------------------\n");
 
-
-	/* For each existing PCB, print valid fields */
+	/* For each existing PCB, we print valid fields */
 	for (i=0; i < num_processes; i++)
 	{
 	    
@@ -80,45 +73,40 @@ void printr() {
 		else printf("\t");
 
 	    printf(" \n");
-
 	}
 	
-
   return;
 }
 
-/***************************************************************/
 void create() {
   
-	/* Declare local variables */
 	int p;
 	int q=1;
 	int next_child;
 	int num_children = 0;
 	
-	/* Prompt for the parent PCB index */
 	printf("Enter the parent process index: ");
 	scanf("%d", &p);
 	
-	/* Search for the next unused PCB index q */
+	/* Searching for the next unused PCB index q */
   while (pcb[q].parent != -1){
 	  q++;
   }
-	/* Record the parent PCB index p in PCB[q] */
+	/* Recording the parent PCB index p in PCB[q] */
 	pcb[q].parent = p;
 
-	/* Initialize the first_child & younger_sibling of PCB[q] */ // ****************** NOT SURE IF THIS IS CORRECT BELOW
+	/* Initializing the first_child & younger_sibling of PCB[q] */ // 
     pcb[q].first_child = -1;
     pcb[q].younger_sibling = -1;
 
-	/* Check if parent PCB has no first child--is so, set fields appropriately */
+	/* Checking to see if parent PCB has no first child--if so, setingt fields appropriately */
 	if (pcb[p].first_child == -1)
 	{
 		pcb[p].first_child = q;
 		num_children = 1;
 	}
 	
-	/* Else, search for appropriate available spot for next child, set fields appropriately */
+	/* Else, we are searching for appropriate available spot for next child, and setting fields appropriately */
 	else {
 		num_children = 2;
 		next_child = pcb[p].first_child;
@@ -132,27 +120,20 @@ void create() {
 	}
 	
 	
-	/* Print message indicating the creation of the ith child of process p at q */
+	/* Printing message indicating the creation of the ith child of process p at q */
 	printf("cr[%d]:\tcreates the %dth child of PCB[%d] at PCB[%d]\n\n", p, num_children, p, q);
-	/* NOTE: do not use the letters i,p,q directly, but the values of each variable */
    
-	/* Print PCB table */
+	/* Printing the PCB table */
 	printr();
+	
   return;
 }
 
 
-/***************************************************************/
 void destroy(int q) {
 
-	/* If the formal parameter is -1, return */ 
 	if(q == -1) return;
-	/* Else:
-		Call this procedure on the current PCB's younger sibling
-		Call this procedure on the current PCB's first child
-		Set all fields to invalid 
-		Print current PCB to be destroyed
-	*/
+	
 	else
 	{
 		destroy(pcb[q].younger_sibling);
@@ -172,42 +153,36 @@ void destroy(int q) {
 /***************************************************************/
 void destroyer() {
   
-	/* Declare local variables */
 	int p;
 	
-	/* Prompt for the parent PCB index p*/
 	printf("Enter the process whose descendants are to be destoryed: ");
 	scanf("%d", &p);
 
-	/* Print message indicating the destruction of the descendants of PCB[p] */
 	printf("de[%d]:\tdestroys all the descendants of PCB[%d] which includes: ", p, p);
 	
-	/* Call recursive procedure on PCB[p]'s first child */
+	/* Recursive call procedure on PCB[p]'s first child */
 	destroy(pcb[p].first_child);
 	
-	/* Set PCB[p]'s first child to invalid */
+	/* Setting PCB[p]'s first child to invalid */
 	pcb[p].first_child = -1;
 
-	/* Print PCB table */
+	/* Printting PCB table */
+	
 	printf("\n");
-//	printr();
 	
   return;
 }
 
 
-/***************************************************************/
 void quit() {
 
-	/* If the dynamic array "PCB" is not NULL, free the memory */
+	/* When the dynamic array "PCB" is not NULL, we are going to free the memory */
 	if (pcb != NULL) free(pcb);
   return;
 }
 
-/***************************************************************/
 int main() {
 
-	/* Declare local variables */
 	int choice;
 	
 	/* Until the user quits, print the menu, prompt for the menu choice, call the appropriate procedure */
